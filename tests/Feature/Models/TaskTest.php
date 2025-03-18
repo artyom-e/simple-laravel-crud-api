966b3b2c8c67fc0a2cca1b21119c342968499302
Artem Yeremenko <a.yeremenko@asbis.com>
2025-03-18 11:32:51 +0200

    Add feature tests for Task model is_completed behavior
    This commit introduces tests for the Task model's is_completed logic. It ensures correct behavior when getting or setting the is_completed property and validates the completed_at field updates accordingly.

---
+++ tests/Feature/Models/TaskTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Models;
+
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+/**
+ * @small
+ */
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testGetIsCompletedTrue(): void
+    {
+        $task = Task::factory()->completed()->create();
+        $this->assertNotNull($task->completed_at);
+        $this->assertTrue($task->is_completed);
+    }
+
+    public function testGetIsCompletedFalse(): void
+    {
+        $task = Task::factory()->incompleted()->create();
+        $this->assertNull($task->completed_at);
+        $this->assertFalse($task->is_completed);
+    }
+
+    public function testSetIsCompletedTrue(): void
+    {
+        $task = Task::factory()->incompleted()->create();
+        $this->assertNull($task->completed_at);
+        $task->update(['is_completed' => true]);
+        $this->assertNotNull($task->completed_at);
+        $this->assertTrue($task->is_completed);
+    }
+
+    public function testSetIsCompletedFalse(): void
+    {
+        $task = Task::factory()->completed()->create();
+        $this->assertNotNull($task->completed_at);
+        $task->update(['is_completed' => false]);
+        $this->assertNull($task->completed_at);
+        $this->assertFalse($task->is_completed);
+    }
+}
